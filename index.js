1d0a34b5c0fca6417e0987cedd53e356981fc6b5
amith-krishnan <amithkrishnan98@gmail.com>
2022-10-23 13:43:02 +0530

    first assignment

---
+++ index.js
@@ -0,0 +1,42 @@
+let handleSubmit = (event) => {
+    event.preventDefault();
+    let name = document.forms["form"]["name"].value.trim();
+    let gender = document.forms["form"]["gender"].value;
+    let studentClass = document.forms["form"]["class"].value;
+    let division = document.forms["form"]["division"].value;
+    let dob = document.forms["form"]["dob"].value;
+    if (name && gender && studentClass && division && dob) {
+      alert(
+        "Name: " +
+          name +
+          "\nGender: " +
+          gender +
+          "\nClass: " +
+          studentClass +
+          "\nDivision: " +
+          division +
+          "\nDob: " +
+          dob
+      );
+      document.forms['form'].reset();
+    }
+    name
+      ? document.getElementById("name").classList.remove("error")
+      : document.getElementById("name").classList.add("error");
+    gender
+      ? document
+          .getElementById("error-message")
+          .classList.remove("error-message-active")
+      : document
+          .getElementById("error-message")
+          .classList.add("error-message-active");
+    studentClass
+      ? document.getElementById("class").classList.remove("error")
+      : document.getElementById("class").classList.add("error");
+    division
+      ? document.getElementById("division").classList.remove("error")
+      : document.getElementById("division").classList.add("error");
+    dob
+      ? document.getElementById("dob").classList.remove("error")
+      : document.getElementById("dob").classList.add("error");
+  };
